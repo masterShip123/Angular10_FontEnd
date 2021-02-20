1c5c80b350d03a6a140ce97f78e9a4a516314f91
ชิบปี้ หน้าไม่มัน <sicfier@hotmail.com>
2021-02-20 00:18:51 -0600

    Firt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  // tslint:disable-next-line:ban-types
+  name: String = 'shitty';
+  // tslint:disable-next-line:whitespace
+  position = ['Admin','HR','IT'];
+  imageUrl = 'https://thestandard.co/wp-content/uploads/2020/02/25-1.jpg';
+  // tslint:disable-next-line:typedef
+  setName(){
+    this.name = 'Yai';
+  }
 }
